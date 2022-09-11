98d96f31a1cf9f3977510be2e27510ae2ae64c40
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-11 09:27:52 -0600

    feat: add first iteration of renderMovieCards method to render movie posters from data/movieData.json

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,48 @@
+import movieData from '../data/movieData.json';
 
 function MovieCard() {
+  const renderMovieCards = (movies) => {
+    return (
+      <div className="movies-container">
+        {
+          movies.map((movie, index) => {
+            return (
+              <div className="movie-container">
+                <img className="movie-image" src={`${movie.poster_path}`} alt="movie-poster" />
+                
+              </div>
+            )
+          })
+        }
+
+      </div>
+    );
+  }
   return (
-  <h1>hello</h1>
-  )
+    <div>
+      <div>{renderMovieCards(movieData.movies)}</div>
+    </div>
+  ) 
+  
+
 }
+  // const renderMovieCards = (movies) => {
+  //   movies.map((movie, index) => {
+  //     <div className="movies-container"
+  //       return (
+  //         <div className="movie-card-container"key={index}>
+  //           <img className="movie-poster" src={movie.poster_path} alt="movie-poster" />
+  //         </div>
+      
+  //   })
+  //   )
+  // }
+
+  // return (
+  //   <>
+  //     {renderMovieCards(movieData.movies)}
+  //   </>
+  
+  // )
 
 export default MovieCard;
