1fc13f5f6bdabd63e31734e3f8539e8cbd96185a
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-13 14:11:33 -0600

    refactor: movie allMovies state to App component, movie allMovies .map to app component and instantiate each MovieCard

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@
 import './App.css';
 import MovieCard from './components/MovieCard.js'
 import Header from './components/Header.js'
+import { useState, useEffect } from 'react'
 
 function App() {
-  
+  const [allMovies, setAllMovies] = useState([])
+
+  useEffect(() => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
+      .then(response => response.json())
+      .then(data => setAllMovies(data.movies))
+  })
+
+  const cards = allMovies.map(movie => {
+    return (
+      <MovieCard
+        key={movie.id}
+        movie={movie}
+      />
+    )
+  })
   return (
-    <>
+    <div className="app">
     <Header />
-      <div className="movies">  
-        <MovieCard />
-      </div>
-    </>
+      <section className="movies">  
+        {cards}
+      </section>
+    </div>
   );
 }
 
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,39 +1,16 @@
 // import movieData from '../data/movieData.json';
 import { useState, useEffect } from 'react'
 
-function MovieCard() {
-  const [allMovies, setAllMovies] = useState([])
-
-  useEffect(() => {
-    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
-    .then(response => response.json())
-    .then(data => setAllMovies(data.movies))
-  })
-
-
-
-  const renderMovieCards = (movies) => {
+function MovieCard(props) {
+console.log(props.movie)
     return (
       <div className="movies-list">
-        {
-          movies.map((movie, index) => {
-            return (
-              <div className="movie-container">
-                <img className="movie-image" src={`${movie.poster_path}`} key="index" alt="movie-poster" />
-                
-              </div>
-            )
-          })
-        }
+        <div className="movie-container">
+          <img className="movie-image" src={`${props.movie.poster_path}`} key="index" alt="movie-poster" />
 
+        </div>
       </div>
     );
-  }
-  return (
-    <div>
-      <div>{renderMovieCards(allMovies)}</div>
-    </div>
-  ) 
 }
 
 export default MovieCard;
