0609211cad658e8b0773e473453b5ac88c6e0f66
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-13 19:06:31 -0600

    feat: add API call to get single movie info in Movie Show component"

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,17 +1,20 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react'
+
 
 function MovieShow(props) {
 
   const { id } = useParams()
-  console.log(id)
+  const [movieInfo, setMovieInfo] = useState({})
+  useEffect(() => {
+    fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+      .then(response => response.json())
+      .then(data => setMovieInfo(data.movie))
+  }, [])
+  console.log(movieInfo)
+
   return (
-    <h1>TEESTT</h1>
+    <h1>{movieInfo.title}</h1>
   )
 }
 
