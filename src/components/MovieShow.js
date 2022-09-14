dbbb8e0d1392cd83e5fcd6d3f071b379ee42b89c
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-13 21:03:15 -0600

    feat: finish styling of whole component, refactor movie year and rating into variables to round and split month/day"

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -5,30 +5,35 @@ import { useState, useEffect } from 'react'
 function MovieShow(props) {
 
   const { id } = useParams()
+
   const [movieInfo, setMovieInfo] = useState({})
+
   useEffect(() => {
     fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
       .then(response => response.json())
       .then(data => setMovieInfo(data.movie))
       console.log('effect ran')
   }, [])
+
   const genres = movieInfo.genres?.join(' · ')
+  const movieRating = movieInfo.average_rating?.toFixed(1)
+  const movieYear = movieInfo.release_date?.split('-', 1) 
   return (
     <div className="movie-component">
       <div className="backdrop-container">
         <img className="movie-backdrop" src={movieInfo.backdrop_path} alt="movie-backdrop" />
         <h1 className="movie-title">{movieInfo.title}</h1>
-        <p className="movie-info-short">{`${genres} ${movieInfo.release_date}`}</p>
+        <p className="movie-info-short">{`${genres} · ${movieYear}`}</p>
       </div>
       <div className="movie-text-container">
         <div className="movie-info-left">
           <h2>About <i>{movieInfo.title}</i></h2>
           
           <p>Release Date</p>
-          <p className="movie-data">{movieInfo.release_date}</p>
+          <p className="movie-data">{movieYear}</p>
           
           <p>Average Rating</p>
-          <p className="movie-data">{movieInfo.average_rating}</p>
+          <p className="movie-data">{movieRating}</p>
 
           <p>Genres</p>
           <p className="movie-data">{genres}</p>
